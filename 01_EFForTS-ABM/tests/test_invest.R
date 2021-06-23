36d6f3b4832e5c0ac3082abc24a5b9a5e0ce0356
nilsbeyer <nils.beyer@stud.uni-goettingen.de>
2021-06-23 23:49:26 +0200

    checking minimal again

--- 01_EFForTS-ABM/tests/test_invest.R
+++ 01_EFForTS-ABM/tests/test_invest.R
@@ -60,7 +60,7 @@ nl <- nl(nlversion = netlogoversion,
 #dummy_list <- list(0)
 #names(dummy_list) <- c("dummy_variable")
 variable_list <- list(3478436)#"general", "server"
-names(variable_list) <- c("rnd-seed")#"biodiv_invest_objective", "which-machine?"
+names(variable_list) <- c("rand-seed")#"biodiv_invest_objective", "which-machine?"
          
 nl@experiment <- experiment(expname="test",
                            outpath=outpath,
