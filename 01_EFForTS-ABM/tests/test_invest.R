6f5e71330be1868f1e08dbc9056791a53a066930
nilsbeyer <nils.beyer@stud.uni-goettingen.de>
2021-06-25 16:33:01 +0200

    checking minimal constants

--- 01_EFForTS-ABM/tests/test_invest.R
+++ 01_EFForTS-ABM/tests/test_invest.R
@@ -60,7 +60,7 @@ nl <- nl(nlversion = netlogoversion,
 #dummy_list <- list(0)
 #names(dummy_list) <- c("dummy_variable")
 variable_list <- list("\"hundred-farmers3\"")#"general", "server"3478436
-names(variable_list) <- c("which-map")#"rand-seed","dummy_variable", "biodiv_invest_objective")#, "which-machine?"
+names(variable_list) <- c(`which-map`)#"rand-seed","dummy_variable", "biodiv_invest_objective")#, "which-machine?"
          
 nl@experiment <- experiment(expname="test",
                            outpath=outpath,
