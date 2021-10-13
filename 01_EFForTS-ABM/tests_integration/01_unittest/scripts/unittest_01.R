c022fcd631fd0911728ee96f06a0f5e375ee46f0
JuliaHen <julia.henzler@uni-goettingen.de>
2021-10-13 11:12:59 +0200

    finished unit test for forest

--- 01_EFForTS-ABM/tests_integration/01_unittest/scripts/unittest_01.R
+++ 01_EFForTS-ABM/tests_integration/01_unittest/scripts/unittest_01.R
@@ -1,7 +1,8 @@
-###unit test for Natcap Invest Integration
-## specify experiment
+#### Unit test for Natcap Invest Integration ####
+## specify experiment and adapt netlogopath, modelpath, outpath and netlogoversion
 
 library(nlrx)
+library(raster)
 experiment <- "test1"
 invtest <- paste("\"",experiment,"\"",sep="")
 netlogopath <- file.path("/home/dockerj/nl")
@@ -38,6 +39,38 @@ print(nl)
 # results <- run_nl_one(nl, seed = 1, siminputrow = 1)
 # for debuggin, keep interim output in \tmp
 results <- run_nl_one(nl,seed = 1, siminputrow = 1, cleanup.csv = FALSE, cleanup.xml = FALSE, cleanup.bat = FALSE)
+
+#### Validation of results: Comparison of expected result  and result quality_c_test1.asc ####
+## Input
+#lulc <- raster(paste("/home/dockerj/EFForTS-ABM/01_EFForTS-ABM/tests_integration/01_unittest/", experiment, "/input/lulc.asc" ,sep=""))
+#plot(lulc)
+#op <- raster(paste("/home/dockerj/EFForTS-ABM/01_EFForTS-ABM/tests_integration/01_unittest/", experiment, "/input/oilpalm_c.asc" ,sep=""))
+#plot(op)
+#rb <- raster(paste("/home/dockerj/EFForTS-ABM/01_EFForTS-ABM/tests_integration/01_unittest/", experiment, "/input/rubber_c.asc" ,sep="")) 
+#plot(rb)
+
+## InVEST result
+investmap <- raster(paste("/home/dockerj/EFForTS-ABM/01_EFForTS-ABM/tests_integration/01_unittest/", experiment, "/output/quality_c_", experiment, ".asc" ,sep=""))
+# Creating dataframe of rasterdata
+#investmap_df <- as.data.frame(investmap, xy = TRUE)
+# Plot raster
+#str(investmap_df)
+#res(investmap)
+#plot(investmap)
+
+## Expected result
+expectedmap <- raster(ncol=100, nrow= 100, xmn=212461, xmx=217461, ymn=9753255, ymx=9758255)
+projection(expectedmap) <- "+proj=utm +zone=48 +south +datum=WGS84"
+values(expectedmap) <- 1
+
+## Comparison
+invest_vs_expected <- function(investmap=investmap,expectedmap=expectedmap){
+ x <- cellStats(investmap, mean)
+ y <- cellStats(expectedmap, mean)
+ if (x == y) {print ("Unittest validated")}  else {"Unittest not validated"}
+}
+
+invest_vs_expected(investmap,expectedmap)  
 
 # additional possible steps to analyze results via NLRX - not required for unit test
 # Attach results to nl object:
