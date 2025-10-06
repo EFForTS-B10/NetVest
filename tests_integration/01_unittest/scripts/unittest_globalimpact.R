3ba7e68596482112aa9ac419aeac1a11e27a620a
Julia Henzler <62095836+JuliaHen@users.noreply.github.com>
2025-10-06 14:29:26 +0200

    Update unittest_globalimpact.R

--- tests_integration/01_unittest/scripts/unittest_globalimpact.R
+++ tests_integration/01_unittest/scripts/unittest_globalimpact.R
@@ -14,6 +14,10 @@ library(tidyverse)
 library(patchwork)
 library(ggpmisc)
 
+# Set JAVA_HOME according to your needs
+# (see details: https://github.com/ropensci/nlrx/issues/32#issuecomment-555475538)
+Sys.setenv(JAVA_HOME = "/usr/lib/jvm/java-11-openjdk-amd64")
+
 ### 1) Unittest execution
 experiment <- "globalimpact"
 invtest <- paste("\"",experiment,"\"",sep="")
